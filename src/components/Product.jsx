b68defd528a08f2f58492003bf55ace2b9cb97fb
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-10-06 10:36:37 -0500

    testing commit config

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
 const Product = ({ product, handleAddToCart }) => (
-  <div className="Products-item">
+  <div className='Products-item'>
     <img src={product.image} alt={product.title} />
-    <div className="Products-item-info">
+    <div className='Products-item-info'>
       <h2>
         {product.title}
-        <span>
-          $
-          {product.price}
-        </span>
+        <span>${product.price}</span>
       </h2>
       <p>{product.description}</p>
     </div>
-    <button type="button" onClick={handleAddToCart(product)}>Comprar</button>
+    <button type='button' onClick={handleAddToCart(product)}>
+      Comprar
+    </button>
   </div>
 );
 
