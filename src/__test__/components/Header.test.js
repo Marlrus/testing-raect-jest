5bbf124a32f945f9d7dba1e29bc6b2d0b2dcc5d0
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-10-20 08:12:39 -0500

    react-test-renderer, create, and snapshot

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import ProviderMock from '../../__mocks__/providerMock';
 import Header from '../../components/Header';
 
@@ -22,5 +23,16 @@ describe('<Header />', () => {
 
   test('Render del Titulo', () => {
     expect(headerMount.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshot', () => {
+  test('Comprobar el Snapshot del Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
